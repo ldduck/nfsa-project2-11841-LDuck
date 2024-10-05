d06c3f61fbf743334f647d774f5808e1ed750bff
ldduck <125320315+ldduck@users.noreply.github.com>
2024-10-05 15:36:20 +1000

    build 2

--- src/components/TheFetch.vue
+++ src/components/TheFetch.vue
@@ -21,31 +21,50 @@ export default {
 
   methods: {
     fetchData() {
+      // Construct the query string using the current search parameters and page number
       let queryString =
         this.query + this.searchString + '&page=' + this.currentPage + '&hasMedia=yes'
+
+      // Make a fetch request to the constructed query string
       fetch(queryString)
         .then((response) => {
+          // Convert the response to JSON
           response.json().then((res) => {
+            // Merge the new data into tempData
             this.$data.tempData = { ...this.$data.tempData, ...res }
+            // Append the new results to tempResultSet
             this.$data.tempResultSet = this.$data.tempResultSet.concat(res.results)
+            // Update the total count of results
             this.$data.total = res.meta.count.total
+
+            // Check if there are any results
             if (this.$data.total > 0) {
+              // Check if we need to fetch more pages (limit to 500 results or total results)
               if (this.currentPage * 25 < 500 && this.currentPage * 25 < this.$data.total) {
+                // Increment the page number
                 this.currentPage++
+                // Fetch the next page of data
                 this.fetchData()
               } else {
+                // If no more pages to fetch, move tempData to theData
                 this.$data.theData = this.$data.tempData
+                // Clear tempData
                 this.$data.tempData = {}
+                // Move tempResultSet to resultSet
                 this.$data.resultSet = this.$data.tempResultSet
+                // Clear tempResultSet
                 this.$data.tempResultSet = []
+                // Reset the page number
                 this.currentPage = 1
               }
             } else {
+              // Log if no results were found
               console.log('no results')
             }
           })
         })
         .catch((err) => {
+          // Log any errors that occur during the fetch
           console.error(err)
         })
     }
@@ -62,8 +81,11 @@ export default {
 
     <ul role="list" class="list-v">
       <li v-for="(result, index) in resultSet" :key="result[index]">
-        <p>{{ result['title'] }}</p>
+        <h2>{{ result['title'] }}</h2>
         <p>{{ result['name'] }}</p>
+        <p>{{ result['id'] }}</p>
+        <p>{{ result['subMedium'] }}</p>
+
         <img
           v-if="result['preview'] && result['preview'][0]"
           v-bind:src="imgURL + result['preview'][0]['filePath']"
