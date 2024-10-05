3c3c915ebb22b747c359a69dbe0a91d2219f03c7
ldduck <125320315+ldduck@users.noreply.github.com>
2024-10-05 15:48:32 +1000

    Hosting Error Rebuild

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nsfa-project2-11841-LDuck/',
+  base: './',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
